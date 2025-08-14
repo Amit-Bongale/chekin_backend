8356fe45d96d62dbcfdc5caa0239f446521f2253
Amit-Bongale <amitbongale14@gmail.com>
2025-08-14 18:06:03 +0530

    user log controller updated

--- src/main/java/com/example/checkin/controller/UsersLogController.java
+++ src/main/java/com/example/checkin/controller/UsersLogController.java
@@ -2,7 +2,6 @@ package com.example.checkin.controller;
 
 import com.example.checkin.models.UsersLog;
 import com.example.checkin.repo.UsersLogRepository;
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
@@ -13,7 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.sql.Date;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -33,34 +32,14 @@ public class UsersLogController {
     }
 
 
-//    @PreAuthorize("hasAnyAuthority('ADMIN' , 'SUPER_ADMIN')")
-//    @GetMapping("/{startDate}/{endDate}")
-//    public List<UsersLog> getVisitorsBetweenDates(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate , @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate){
-//
-//        LocalDateTime start = startDate.atStartOfDay();
-//        LocalDateTime end = endDate.atTime(LocalTime.MAX);
-//
-//        System.out.println("Start:" + start + "End:" + end );
-//        return usersLogRepository.findAllByLoginTimeBetween(start , end);
-//
-//    }
-
     @PreAuthorize("hasAnyRole('ADMIN' , 'SUPER_ADMIN')")
     @GetMapping(value = "/{startdate}/{enddate}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<UsersLog>> getVisitorsBetweenDates(
             @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startdate,
-            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate enddate,
-            HttpServletRequest request) {
-
-        System.out.println("=== DEBUG INFO ===");
-        System.out.println("Request URL: " + request.getRequestURL());
-        System.out.println("Start date: " + startdate);
-        System.out.println("End date: " + enddate);
+            @PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate enddate) {
 
         LocalDateTime start = startdate.atStartOfDay();
         LocalDateTime end = enddate.atTime(LocalTime.MAX);
-
-        System.out.println("Start DateTime:" + start + " End DateTime:" + end);
 
         List<UsersLog> result = usersLogRepository.findAllByLoginTimeBetween(start, end);
         return ResponseEntity.ok(result);
